ca929fc3110159e0f091c6899a6661813454e554
Alexey <lp@tdsgn.ru>
2026-06-11 18:26:11 +0700

    chore: upgrade core stack to laravel 12, phpstan 2, pest 3 and register kafka package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use Mateusjunges\Kafka\Providers\LaravelKafkaServiceProvider;
 
 return [
     AppServiceProvider::class,
+    LaravelKafkaServiceProvider::class,
 ];
